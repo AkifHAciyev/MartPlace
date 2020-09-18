23ad3577f689b7e7d793b3a31e0f0e9487c138e7
AkifHAciyev <akif6563@mail.ru>
2020-09-18 16:33:02 +0400

    added home Work №11

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,19 @@
 $(function () {
 
+	$(".rate-star").rateYo({
+		rating: 4.4,
+		starWidth: "17px",
+		readOnly: true,
+	});
 
+	$('.slider__slider-inner').slick({
+		variableWidth: true,
+	});
 
+	$('.slider__slider').css('display', 'flex');
 
+	var mixer = mixitup('.newest-products__inner-box');
+
+	const burger = document.querySelector('.newest-products__burger');
 
 });
