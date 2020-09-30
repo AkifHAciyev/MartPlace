0e5e7fab9ab2d8d3928c9cf209df289a3170064e
AkifHAciyev <akif6563@mail.ru>
2020-09-30 04:11:50 +0400

    addet lesson 11

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 $(function () {
 
+
 	$(".rate-star").rateYo({
 		rating: 4.4,
 		starWidth: "17px",
@@ -12,8 +13,26 @@ $(function () {
 
 	$('.slider__slider').css('display', 'flex');
 
+
+	$('input[type="file"], select').styler();
+
 	var mixer = mixitup('.newest-products__inner-box');
 
 	const burger = document.querySelector('.newest-products__burger');
+
+
+
+	$('.followers__slider').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1
+	});
+
+	$('.feedback__items').slick({
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1
+	});
+
 
 });
